b9b7a8f4bdb4889db40bdbb5c91444b5cc30e160
Giovanni Biancuzzo <giovannibiancuzzo.dev@gmail.com>
2022-10-03 16:39:06 +0200

    definizione componente ricerca

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import LogicComponent from './components/LogicComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App () {
   return (
