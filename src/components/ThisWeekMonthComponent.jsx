a81e794c3c451e30af9e85c581203025183abc38
Giovanni Biancuzzo <giovannibiancuzzo.dev@gmail.com>
2022-10-06 10:26:40 +0200

    fix data not yet acquired

--- src/components/ThisWeekMonthComponent.jsx
+++ src/components/ThisWeekMonthComponent.jsx
@@ -5,12 +5,17 @@ import MonthDetails from "./MonthDetails";
 import WeekElementCarousel from "./WeekElementCarousel";
 
 const ThisWeekMonthComponent = () => {
-    const actualCity = useSelector(
-        (state) => state.weatherInfos.actualCity.list
-    );
+    // const actualCity = useSelector(
+    //     (state) => state.weatherInfos.actualCity.list
+    // );
 
-    const actualCityWeek = actualCity.filter((el, index) => index % 8 === 0);
-    const [actualCityDay, setActualCityDay] = useState(actualCityWeek[0]);
+    const actualCityWeek = useSelector((state) =>
+        state.weatherInfos.actualCity.list?.filter(
+            (el, index) => index % 8 === 0
+        )
+    ); //visto che ogni 8 elementi fetchati abbiamo un ciclo di 24 ore
+
+    const [actualCityDay, setActualCityDay] = useState(null);
 
     const [key, setKey] = useState("thisWeek");
 
@@ -21,82 +26,90 @@ const ThisWeekMonthComponent = () => {
             onSelect={(k) => setKey(k)}
             className=" titles thisWeekMonthContainer"
         >
-            <Tab
-                eventKey="thisWeek"
-                title="This week"
-                className="mainGradient shadowCorners"
-            >
-                <Carousel>
-                    {/* {actualCityWeek.map((pages, index) => (
-                        <Carousel.Item
-                            className="mainGradient shadowCorners"
-                            key={index}
-                        >
+            {actualCityWeek ? (
+                <Tab
+                    eventKey="thisWeek"
+                    title="This week"
+                    className="mainGradient shadowCorners"
+                >
+                    <Carousel>
+                        {/* {actualCityWeek.map((pages, index) => (
+                            <Carousel.Item
+                                className="mainGradient shadowCorners"
+                                key={index}
+                            >
+                                <Row>
+                                    {actualCityWeek
+                                        .slice(
+                                            Math.floor(index / 3) * 3,
+                                            Math.floor(index / 3) * 3 + 3
+                                        )
+                                        .map((day, indexCarousel) => (
+                                            <WeekElementCarousel
+                                                day={day}
+                                                key={day.dt}
+                                            />
+                                        ))}
+                                </Row>
+                            </Carousel.Item>
+                        ))} */}
+                        <Carousel.Item className="mainGradient shadowCorners">
                             <Row>
                                 {actualCityWeek
-                                    .slice(
-                                        Math.floor(index / 3) * 3,
-                                        Math.floor(index / 3) * 3 + 3
-                                    )
+                                    .slice(0, 3)
                                     .map((day, indexCarousel) => (
-                                        <WeekElementCarousel
-                                            day={day}
-                                            key={day.dt}
-                                        />
+                                        <Col
+                                            xs={4}
+                                            onClick={() => {
+                                                setActualCityDay(day);
+                                                setKey("thisMonth");
+                                            }}
+                                        >
+                                            <WeekElementCarousel
+                                                day={day}
+                                                key={day.dt}
+                                            />
+                                        </Col>
                                     ))}
                             </Row>
                         </Carousel.Item>
-                    ))} */}
-                    <Carousel.Item className="mainGradient shadowCorners">
-                        <Row>
-                            {actualCityWeek
-                                .slice(0, 3)
-                                .map((day, indexCarousel) => (
-                                    <Col
-                                        xs={4}
-                                        onClick={() => {
-                                            setActualCityDay(day);
-                                            setKey("thisMonth");
-                                        }}
-                                    >
-                                        <WeekElementCarousel
-                                            day={day}
-                                            key={day.dt}
-                                        />
-                                    </Col>
-                                ))}
-                        </Row>
-                    </Carousel.Item>
-                    <Carousel.Item className="mainGradient shadowCorners">
-                        <Row>
-                            {actualCityWeek
-                                .slice(3, 6)
-                                .map((day, indexCarousel) => (
-                                    <Col
-                                        xs={4}
-                                        onClick={() => {
-                                            setActualCityDay(day);
-                                            setKey("thisMonth");
-                                        }}
-                                    >
-                                        <WeekElementCarousel
-                                            day={day}
-                                            key={day.dt}
-                                        />
-                                    </Col>
-                                ))}
-                        </Row>
-                    </Carousel.Item>
-                </Carousel>
-            </Tab>
-            <Tab
-                eventKey="thisMonth"
-                title="This month"
-                className="mainGradient shadowCorners p-4"
-                style={{ height: "37vh" }}
-            >
-                <MonthDetails day={actualCityDay} />
-            </Tab>
+                        <Carousel.Item className="mainGradient shadowCorners">
+                            <Row>
+                                {actualCityWeek
+                                    .slice(3, 6)
+                                    .map((day, indexCarousel) => (
+                                        <Col
+                                            xs={4}
+                                            onClick={() => {
+                                                setActualCityDay(day);
+                                                setKey("thisMonth");
+                                            }}
+                                        >
+                                            <WeekElementCarousel
+                                                day={day}
+                                                key={day.dt}
+                                            />
+                                        </Col>
+                                    ))}
+                            </Row>
+                        </Carousel.Item>
+                    </Carousel>
+                </Tab>
+            ) : (
+                <span>Caricamento</span>
+            )}
+            {actualCityDay ? (
+                <Tab
+                    eventKey="thisMonth"
+                    title="This month"
+                    className="mainGradient shadowCorners p-4"
+                    style={{ height: "37vh" }}
+                >
+                    <MonthDetails day={actualCityDay} />
+                </Tab>
+            ) : (
+                <span>Caricamento</span>
+            )}
         </Tabs>
     );
 };
