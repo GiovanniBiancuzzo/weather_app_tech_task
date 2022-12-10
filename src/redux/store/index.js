a65df3e3c213e0dbf9d44b701555e06f4379c194
Giovanni Biancuzzo <giovannibiancuzzo.dev@gmail.com>
2022-12-10 18:47:18 +0100

    implemented expiration date on data in the redux store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,16 +4,21 @@ import favouritesReducer from "../reducers/favouritesReducer";
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import expireIn from "redux-persist-transform-expire-in";
 
 const mainReducer = combineReducers({
     weatherInfos: weatherInfosReducer,
     favourites: favouritesReducer,
 });
 
+const expireTime = 48 * 60 * 60 * 1000; // expire in 48h
+const expirationKey = "expirationKey";
 const persistConfig = ({
     key: 'root',
     storage,
-    blacklist: 'weatherInfos'
+    blacklist: 'weatherInfos',
+    transforms: [expireIn(expireTime, expirationKey, [])]
+
 });
 
 const persistedReducer = persistReducer(persistConfig, mainReducer);
